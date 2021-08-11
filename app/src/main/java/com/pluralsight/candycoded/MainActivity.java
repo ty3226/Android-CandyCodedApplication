f8d06492afcacce356dcf951d077a876166d8e30
ty3226 <47489814+ty3226@users.noreply.github.com>
2021-08-11 15:47:11 -0400

    Added Share Activity Intent

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -84,6 +84,13 @@ public class MainActivity extends AppCompatActivity {
   // ***
   // TODO - Task 1 - Show Store Information Activity
   // ***
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    Intent infoIntent = new Intent(this, InfoActivity.class);
+    startActivity(infoIntent);
+
+    return super.onOptionsItemSelected(item);
+  }
 
   private void addCandiesToDatabase(Candy[] candies) {
     SQLiteDatabase db = candyDbHelper.getWritableDatabase();
@@ -97,13 +104,5 @@ public class MainActivity extends AppCompatActivity {
 
       db.insert(CandyContract.CandyEntry.TABLE_NAME, null, values);
     }
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-    Intent infoIntent = new Intent(this, InfoActivity.class);
-    startActivity(infoIntent);
-
-    return super.onOptionsItemSelected(item);
   }
 }
